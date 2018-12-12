ba1d45a9fdd3a9743560ed84526cf6727f22796b
Matt Davis <matthew.davis@outlook.com>
2018-12-12 00:56:36 -0500

    add .env

--- controllers/image.js
+++ controllers/image.js
@@ -1,14 +1,17 @@
+require('dotenv').config();
 const Clarifai = require('clarifai');
 
 //You must add your own API key here from Clarifai.
+
 const app = new Clarifai.App({
- apiKey: 'YOUR_API_KEY_HERE'
+  apiKey: process.env.CLARIFAI
 });
 
 const handleApiCall = (req, res) => {
   app.models
     .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
     .then(data => {
+      console.log(data)
       res.json(data);
     })
     .catch(err => res.status(400).json('unable to work with API'))
@@ -17,12 +20,12 @@ const handleApiCall = (req, res) => {
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db('users').where('id', '=', id)
-  .increment('entries', 1)
-  .returning('entries')
-  .then(entries => {
-    res.json(entries[0]);
-  })
-  .catch(err => res.status(400).json('unable to get entries'))
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+      res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json('unable to get entries'))
 }
 
 module.exports = {
